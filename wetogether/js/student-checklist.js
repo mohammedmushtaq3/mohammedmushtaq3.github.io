6c140679e357a994f988f9bbe7f110167c34a4a9
overlord-mmorpg <58843133+overlord-mmorpg@users.noreply.github.com>
2021-06-11 12:52:35 +0530

    updates
    added student checklist data

--- wetogether/js/student-checklist.js
+++ wetogether/js/student-checklist.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6 */
 
-let default_survey = {
+let student_checklist = {
   statusCode: 200,
   body: {
     created: "2021-06-10T22:48:01.169Z",
@@ -18,8 +18,6 @@ let default_survey = {
               { label: { en: "Much worse than usual", ar: "", value: 4 } },
             ],
           },
-        ],
-        questions: [
           {
             en: "Have you recently been feeling in need of a good tonic?",
             ar: "",
@@ -30,8 +28,6 @@ let default_survey = {
               { label: { en: "Much more than usual", ar: "", value: 4 } },
             ],
           },
-        ],
-        questions: [
           {
             en: "Have you recently been feeling rundown and out of sorts",
             ar: "",
@@ -40,6 +36,256 @@ let default_survey = {
               { label: { en: "No more than usual", ar: "", value: 2 } },
               { label: { en: "Rather more than usual", ar: "", value: 3 } },
               { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently felt that you are ill?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been geeting any pains in your head?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been getting a feeling of tightness or pressure in your head?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been having hot or cold spells?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently lost much sleep over worry?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently had difficulty in staying asleep once your are off?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been managing to keep yourself busy and occupied?",
+            ar: "",
+            labels: [
+              { label: { en: "More so than usual", ar: "", value: 1 } },
+              { label: { en: "Same as usual", ar: "", value: 2 } },
+              { label: { en: "Less so than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been taking longer over the things you do?",
+            ar: "",
+            labels: [
+              { label: { en: "Quicker than usual", ar: "", value: 1 } },
+              { label: { en: "Same as usual", ar: "", value: 2 } },
+              { label: { en: "Longer than usual", ar: "", value: 3 } },
+              { label: { en: "Much longer than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently felt on the whole youwere doing things well?",
+            ar: "",
+            labels: [
+              { label: { en: "Better than usual", ar: "", value: 1 } },
+              { label: { en: "About the same", ar: "", value: 2 } },
+              { label: { en: "Less well than usual", ar: "", value: 3 } },
+              { label: { en: "Much less well", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been satisfied with the wayyou've carried out your task?",
+            ar: "",
+            labels: [
+              { label: { en: "More satisfied", ar: "", value: 1 } },
+              { label: { en: "About same as usual", ar: "", value: 2 } },
+              { label: { en: "Less satisfied than usual", ar: "", value: 3 } },
+              { label: { en: "Much less satisfied", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently felt that you are playinga useful part in things?",
+            ar: "",
+            labels: [
+              { label: { en: "More so than usual", ar: "", value: 1 } },
+              { label: { en: "Same as usual", ar: "", value: 2 } },
+              { label: { en: "Less so than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently felt capable of making decisions about things?",
+            ar: "",
+            labels: [
+              { label: { en: "More so than usual", ar: "", value: 1 } },
+              { label: { en: "Same as usual", ar: "", value: 2 } },
+              { label: { en: "Less so than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently felt constantly understrain?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been able to enjoy yournormal day-to-day activities?",
+            ar: "",
+            labels: [
+              { label: { en: "More so than usual", ar: "", value: 1 } },
+              { label: { en: "Same as usual", ar: "", value: 2 } },
+              { label: { en: "Less so than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been getting edgy andbad-tempered?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been getting scared or panicky for no good reason?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently found everything gettingon top of you?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been thinking of yourselfas a worthless person?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently felt that life is entirelyhopeless?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently been feeling nervous astrung-up all the time?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently felt that life isn't worthliving?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently thought of the possibility that you might make away with yourself?",
+            ar: "",
+            labels: [
+              { label: { en: "Definitly not", ar: "", value: 1 } },
+              { label: { en: "I don't think so", ar: "", value: 2 } },
+              { label: { en: "Has crossed my mind", ar: "", value: 3 } },
+              { label: { en: "Definitely has", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently found at times you couldn't do anything because your nerves were too bad?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently found yourself wishing you were dead and away from it all?",
+            ar: "",
+            labels: [
+              { label: { en: "Not at all", ar: "", value: 1 } },
+              { label: { en: "No more than usual", ar: "", value: 2 } },
+              { label: { en: "Rather more than usual", ar: "", value: 3 } },
+              { label: { en: "Much more than usual", ar: "", value: 4 } },
+            ],
+          },
+          {
+            en: "Have you recently found that the idea of taking your own life keptcoming into your mind?",
+            ar: "",
+            labels: [
+              { label: { en: "Definitly not", ar: "", value: 1 } },
+              { label: { en: "I don't think so", ar: "", value: 2 } },
+              { label: { en: "Has crossed my mind", ar: "", value: 3 } },
+              { label: { en: "Definitely has", ar: "", value: 4 } },
             ],
           },
         ],
